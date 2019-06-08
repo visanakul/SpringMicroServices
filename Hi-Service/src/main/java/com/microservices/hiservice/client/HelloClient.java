f5d00d03a4d5d47761e43ffecfe5f89cb0f42da1
VISHAL <visanakul@gmail.com>
2019-06-08 18:24:05 +0530

    Microservices Intercommunication completed

--- Hi-Service/src/main/java/com/microservices/hiservice/client/HelloClient.java
+++ Hi-Service/src/main/java/com/microservices/hiservice/client/HelloClient.java
@@ -1,9 +1,10 @@
 package com.microservices.hiservice.client;
 
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-
-@FeignClient(name="helloClient",url="/hello/sayHello")
+@EnableDiscoveryClient
+@FeignClient(name="helloClient",url = "http://localhost:4444/api/hello")
 public interface HelloClient {
 	@GetMapping("/hello/sayHello")
 	public String getHelloMessage();
